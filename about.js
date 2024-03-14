22c8b040d81833a3f2a17101cf29ec6b7a6ab7a5
aishemporium <ibrahimaishat255@gmail.com>
2024-03-14 15:17:39 +0100

    submission

--- about.js
+++ about.js
@@ -1,17 +1,18 @@
-// const menu = document.querySelector("menu");
-// const hamburger = document.querySelector(".hamburger");
-// const menuIcon = document.querySelector(".menu-icon");
-// const closeIcon = document.querySelector(".close-icon");
+const menu=document.querySelector('.menu');
+const hamburger=document.querySelector('.hamburger');
+const menuIcon=document.querySelector('.menu-icon');
+const closeIcon=document.querySelector('.close-icon');
 
-// function toggleMenu() {
-//     if (menu.classList.contains('show-menu')) {
-//         menu.classList.remove("show-menu");
-//         menuIcon.style.display = "block";
-//         closeIcon.style.display = 'none';
-//     } else {
-//         menu.classList.add ('show-menu');
-//         menuIcon.style.display ="none";
-//         closeIcon.style.display = "block";
-//     }
-// }
-//     hamburger.addEventListener("click", toggleMenu);
+
+function toggleMenu(){
+    if (menu.classList.contains('show-menu')){
+        menu.classList.remove('show-menu');
+        menuIcon.style.display='none'
+        closeIcon.style.display='block'
+    }else{
+        menu.classList.add('show-menu')
+        menuIcon.style.display='block'
+        closeIcon.style.display='none'
+    }
+}
+hamburger.addEventListener("click",toggleMenu)
