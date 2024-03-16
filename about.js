b03a5af24e8f4c10a9b59aa5b71bae5c19c22caa
aishemporium <ibrahimaishat255@gmail.com>
2024-03-16 10:38:36 +0100

    update

--- about.js
+++ about.js
@@ -7,12 +7,12 @@ const closeIcon=document.querySelector('.close-icon');
 function toggleMenu(){
     if (menu.classList.contains('show-menu')){
         menu.classList.remove('show-menu');
-        menuIcon.style.display='none'
-        closeIcon.style.display='block'
-    }else{
-        menu.classList.add('show-menu')
         menuIcon.style.display='block'
         closeIcon.style.display='none'
+    }else{
+        menu.classList.add('show-menu')
+        menuIcon.style.display='none'
+        closeIcon.style.display='block'
     }
 }
 hamburger.addEventListener("click",toggleMenu)
